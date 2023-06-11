b4abcaf8d58b8a74e41a6e2b60397d2ff3699dca
Clara Chabanne <clara.chabanne@gmail.com>
2023-06-11 20:35:02 +0200

    changed to mongoDB uri

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_LOCAL);
+mongoose.connect(process.env.MONGODB_URI);
 
 // Routes
 const offersRoute = require("./routes/offer.route");
